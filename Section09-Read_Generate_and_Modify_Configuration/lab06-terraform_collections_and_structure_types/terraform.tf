e08374d116573b250fc19e08388bb95ee047782f
anas-khan2000 <mak.bvb@gmail.com>
2026-01-19 11:08:14 +0000

    Added terraform collections and structure types

---
+++ Section09-Read_Generate_and_Modify_Configuration/lab06-terraform_collections_and_structure_types/terraform.tf
@@ -0,0 +1,25 @@
+terraform {
+  backend "remote" {
+    organization = "makorg"
+    workspaces {
+      name = "my-aws-app"
+    }
+  }
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+    http = {
+      source  = "hashicorp/http"
+      version = "2.1.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.1.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "2.1.0"
+    }
+  }
+}
